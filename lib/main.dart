a6f3837ccb0f7757689a6a49f3340fd5b1985f54
hirupiyumika <hirupiyumika@gmail.com>
2022-03-13 03:15:34 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:lab4/pages/home.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'IT18206524',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: Scaffold(
+          appBar: AppBar(
+            title: const Center(
+                child: Text(
+              "Agrify",
+            )),
+          ),
+          body: Home()),
+    );
+  }
+}
